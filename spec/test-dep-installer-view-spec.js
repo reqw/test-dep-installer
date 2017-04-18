dae51b24ea0fe3cf56c7a6332ff79baef412f22f
reqw <unk>
2017-04-18 17:51:27 +0300

    remove

--- spec/test-dep-installer-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import TestDepInstallerView from '../lib/test-dep-installer-view';
-
-describe('TestDepInstallerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
