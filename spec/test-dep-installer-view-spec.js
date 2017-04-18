e6ac9dc28c8448d183e5b64f3125931c54ed39dc
reqw <unk>
2017-04-18 17:44:27 +0300

    Initial

---
+++ spec/test-dep-installer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestDepInstallerView from '../lib/test-dep-installer-view';
+
+describe('TestDepInstallerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
